e68c0d58c3bdc2a59fb506cf5a12b12c51077ef1
visualstudio <visualstudio@230-PC41>
2019-02-04 17:02:32 -0500

    Last commit

--- C0741350/js/main.js
+++ C0741350/js/main.js
@@ -1,4 +1,2 @@
-﻿function peanut() {
-    var date = new Date();
-    document.body.innerHTML = "<h1>the date is " + date + "</h1>"
-}
+﻿var date = new Date();
+document.body.innerHTML = "<h1>the date is " + date + "</h1>"
